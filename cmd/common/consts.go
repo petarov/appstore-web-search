fb1a6f75aa4bbde2efd109c5c66a438437e7c13e
Petar Petrov <ppetrov@vexelon.net>
2021-03-20 22:06:09 +0100

    Add PoC search & json results callback

--- cmd/common/consts.go
+++ cmd/common/consts.go
@@ -1,3 +1,4 @@
 package common
 
 const APP_VERSION = "1.0"
+const APP_USER_AGENT = "itunes-web-search"
--- cmd/wasm/itunes.go
+++ cmd/wasm/itunes.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -9,15 +8,13 @@ import (
 	"syscall/js"
 	"time"
 
-	"golang.org/x/net/http2"
-
 	"github.com/petarov/itunes-web-search/cmd/common"
 )
 
 func search(term string, country string, lang string, media string, entity string, client *http.Client) (status int, json string, err error) {
 	query := fmt.Sprintf(
-		"https://itunes.apple.com/search?media=%s&entity=%s&term=%s&country=%s&lang=%s&limit=%d",
-		media, entity, term, country, lang, 25)
+		"https://itunes.apple.com/search?media=%s&entity=%s&term=%s&country=%s&lang=%s&limit=%d&callback=_cb",
+		media, entity, term, country, lang, 10)
 
 	fmt.Printf("Search: %s\n", query)
 
@@ -25,7 +22,7 @@ func search(term string, country string, lang string, media string, entity strin
 	if err != nil {
 		return -1, "", fmt.Errorf("Error creating GET request: %v", err)
 	}
-	req.Header.Set("User-Agent", fmt.Sprintf("itunes-web-search-v%s", common.APP_VERSION))
+	req.Header.Set("User-Agent", fmt.Sprintf("%s-v%s", common.APP_USER_AGENT, common.APP_VERSION))
 
 	reqOut, err := httputil.DumpRequest(req, false)
 	fmt.Println(string(reqOut))
@@ -47,51 +44,26 @@ func search(term string, country string, lang string, media string, entity strin
 func main() {
 	fmt.Println("*** Welcome to iTunes Web Search ***")
 
-	// status, json, err := search("midpoints", "de", "de_DE", "all", "", client)
-	// if err != nil {
-	// 	fmt.Printf("Error in search: %v", err)
-	// 	return
-	// }
-
 	// fmt.Printf("STATUS: %d\n", status)
 	// fmt.Printf("JSON: %s\n", json)
 
-	// stopButton := js.Global().Get("document").Call("getElementById", "stop")
-	// stopButton.Set("onclick", js.FuncOf(func(js.Value, []js.Value) interface{} {
-	// 	go func() {
-	// 		client := &http.Client{Timeout: 5 * time.Second}
-	// 		client.Transport = &http2.Transport{TLSClientConfig: &tls.Config{}}
-
-	// 		_, json, err := search("midpoints", "de", "de_DE", "all", "", client)
-	// 		if err != nil {
-	// 			fmt.Printf("Error in search: %v", err)
-	// 			return
-	// 		}
-
-	// 		fmt.Printf("JSON: %s\n", json)
-	// 	}()
-
-	// 	return nil
-	// }))
-
-	example := func() js.Func {
+	search := func() js.Func {
 		return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 			go func() {
+				cb := args[1]
 				client := &http.Client{Timeout: 5 * time.Second}
-				client.Transport = &http2.Transport{TLSClientConfig: &tls.Config{}}
 
-				_, json, err := search("midpoints", "de", "de_DE", "all", "", client)
+				_, json, err := search(args[0].String(), "de", "de_DE", "all", "", client)
 				if err != nil {
 					fmt.Printf("Error in search: %v", err)
 					return
 				}
-
-				fmt.Printf("JSON: %s\n", json)
+				cb.Invoke(js.Null(), json)
 			}()
 			return ""
 		})
 	}
-	js.Global().Set("example", example())
+	js.Global().Set("search", search())
 
 	select {}
 }
