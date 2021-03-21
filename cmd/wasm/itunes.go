bd8a97088dcd13f2b3a331b06fae78d1b830eec6
Petar Petrov <ppetrov@vexelon.net>
2021-03-21 15:14:41 +0100

    Add wasm error handling

--- cmd/wasm/itunes.go
+++ cmd/wasm/itunes.go
@@ -10,10 +10,10 @@ import (
 	"github.com/petarov/itunes-web-search/cmd/common"
 )
 
-func search(term string, country string, lang string, media string, entity string, client *http.Client) (status int, json string, err error) {
+func search(term string, country string, lang string, media string, entity string, client *http.Client) (json string, err error) {
 	query := fmt.Sprintf(
 		"https://itunes.apple.com/search?media=%s&term=%s&country=%s&limit=%d&callback=_cb",
-		media, term, country, 10)
+		media, term, country, 20)
 
 	if lang != "" {
 		query = fmt.Sprintf("%s&lang=%s", query, lang)
@@ -26,7 +26,7 @@ func search(term string, country string, lang string, media string, entity strin
 
 	req, err := http.NewRequest("GET", query, nil)
 	if err != nil {
-		return -1, "", fmt.Errorf("Error creating GET request: %v", err)
+		return "", fmt.Errorf("Error creating GET request: %v", err)
 	}
 	req.Header.Set("User-Agent", fmt.Sprintf("%s-v%s", common.APP_USER_AGENT, common.APP_VERSION))
 
@@ -35,16 +35,20 @@ func search(term string, country string, lang string, media string, entity strin
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return -1, "", fmt.Errorf("Error in HTTP request: %v", err)
+		return "", fmt.Errorf("Error in HTTP request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return -1, "", fmt.Errorf("Error reading HTTP response body: %v", err)
+		return "", fmt.Errorf("Error reading HTTP response body: %v", err)
 	}
 
-	return resp.StatusCode, string(body), nil
+	if resp.StatusCode/100 != 2 {
+		return string(body), fmt.Errorf("HTTP Error %d", resp.StatusCode)
+	}
+
+	return string(body), nil
 }
 
 func main() {
@@ -58,13 +62,14 @@ func main() {
 				media := args[2].String()
 				cb := args[3]
 
-				_, json, err := search(term, country, "", media, "",
+				json, err := search(term, country, "", media, "",
 					&http.Client{Timeout: 4 * time.Second})
 				if err != nil {
-					fmt.Printf("Error in search: %v", err)
+					cb.Invoke(err.Error(), json)
 					return
+				} else {
+					cb.Invoke(js.Null(), json)
 				}
-				cb.Invoke(js.Null(), json)
 			}()
 			return nil
 		})
