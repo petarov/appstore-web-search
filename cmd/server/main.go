7f27ac56e9328250ad7bd1b0a5f559004ce4c184
Petar Petrov <ppetrov@vexelon.net>
2021-03-21 17:08:36 +0100

    Validate web app path when starting server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/NYTimes/gziphandler"
@@ -17,7 +18,7 @@ var (
 )
 
 func init() {
-	flag.StringVar(&ListenAddress, "address", "localhost", "Server listen address")
+	flag.StringVar(&ListenAddress, "address", "", "Server listen address")
 	flag.IntVar(&ListenPort, "port", 5360, "Server listen port")
 	flag.StringVar(&WebAppPath, "webapp", "./webapp", "Path to the webapp")
 }
@@ -29,6 +30,10 @@ func main() {
 	fmt.Printf("Listening on %s:%d ...\n", ListenAddress, ListenPort)
 
 	WebAppPath, _ = filepath.Abs(WebAppPath)
+	if _, err := os.Stat(WebAppPath); err != nil {
+		fmt.Printf("Webapp path not found: %v", err)
+		return
+	}
 	fmt.Printf("Serving webapp from: %s ...\n", WebAppPath)
 
 	err := http.ListenAndServe(fmt.Sprintf("%s:%d", ListenAddress, ListenPort),
